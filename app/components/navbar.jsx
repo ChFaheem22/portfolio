7d2481f7003d6d5136fb0316d86cd62077e556a9
ChFaheem22 <faheemchh779@gmail.com>
2025-07-17 23:56:57 +0500

    First push

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,10 +3,15 @@ import  Link  from 'next/link'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link href="/">Home</Link>
+      <div className='flex'>
+      <p>Faheem Portfolio</p>
+      </div>
+      <div className='flex'>
+        <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/projects">Projects</Link>
       <Link href="/contact">Contacts</Link>
+      </div>
     </nav>
   )
 }
--- app/page.js
+++ app/page.js
@@ -5,14 +5,14 @@ const Home = () => {
   return (
     <div className="home">
       <div className='bio'>
-      <h1>Hi there, <br /> I&rsquo;sm <span className='name'>Faheem</span> <br />
+      <h1>Hi there, <br /> I&rsquo;m <span className='name'>Faheem</span> <br />
       Frontend Developer</h1>
       <Link className="btn1" href="/about">About Me</Link>
       <Link className="btn1" href="/projects">Projects</Link>
       <Link className="btn1" href="/contact">Contacts</Link>
       </div>
       <div>
-      <Image src="/cropped.png" alt="My Image" className='profile'/>
+      <Image src="/cropped.png" alt="My Image" className='profile' width={300} height={300}/>
       </div>
     </div>
   )
