19d15715f0371ca67ed4e2b455156c4bc6d5101e
ChFaheem22 <faheemchh779@gmail.com>
2025-07-17 22:33:28 +0500

    First push

--- app/about/page.js
+++ app/about/page.js
@@ -2,13 +2,13 @@ const About = () => {
   return (
     <div className="about">
       <div  className="box">
-      <img src="pic.jpg" alt="My Pic" className="img"/>
+      <Image src="pic.jpg" alt="My Pic" className="img"/>
       </div>
 
       <div className="box">
       <h1 className="h1">About Me</h1>
       <p>
-        I'm Faheem, a frontend developer with a passion for creating interactive and user-friendly web interfaces. I'm currently expanding my skills in modern web development by learning React and Next.js. I enjoy building responsive, accessible websites and constantly seek to improve both my design sense and technical abilities. My core technologies include HTML, CSS, JavaScript, React, and Next.js.
+        I&rsquo;sm Faheem, a frontend developer with a passion for creating interactive and user-friendly web interfaces. I&rsquo;sm currently expanding my skills in modern web development by learning React and Next.js. I enjoy building responsive, accessible websites and constantly seek to improve both my design sense and technical abilities. My core technologies include HTML, CSS, JavaScript, React, and Next.js.
       </p>
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className="home">
       <div className='bio'>
-      <h1>Hi there, <br /> I'm <span className='name'>Faheem</span> <br />
+      <h1>Hi there, <br /> I&rsquo;sm <span className='name'>Faheem</span> <br />
       Frontend Developer</h1>
       <Link className="btn1" href="/about">About Me</Link>
       <Link className="btn1" href="/projects">Projects</Link>
       <Link className="btn1" href="/contact">Contacts</Link>
       </div>
       <div>
-      <img src="/cropped.png" alt="My Image" className='profile'/>
+      <Image src="/cropped.png" alt="My Image" className='profile'/>
       </div>
     </div>
   )
