18f8f956f00656d9cb33b17472216ad4fef0463d
ChFaheem22 <faheemchh779@gmail.com>
2025-07-18 06:28:25 +0500

    Portfolio commit

--- app/about/page.js
+++ app/about/page.js
@@ -10,7 +10,7 @@ const About = () => {
       <div className="box">
       <h1 className="h1">About Me</h1>
       <p>
-        I&rsquo;m Faheem, a frontend developer with a passion for creating interactive and user-friendly web interfaces. I&rsquo;sm currently expanding my skills in modern web development by learning React and Next.js. I enjoy building responsive, accessible websites and constantly seek to improve both my design sense and technical abilities. My core technologies include HTML, CSS, JavaScript, React, and Next.js.
+        I&rsquo;m Faheem, a frontend developer with a passion for creating interactive and user-friendly web interfaces. I&rsquo;m currently expanding my skills in modern web development by learning React and Next.js. I enjoy building responsive, accessible websites and constantly seek to improve both my design sense and technical abilities. My core technologies include HTML, CSS, JavaScript, React, and Next.js.
       </p>
       </div>
     </div>
