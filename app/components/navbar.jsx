b3d4eb5f5b3ecb947edcf7d9bb71aa4d47c783d3
ChFaheem22 <faheemchh779@gmail.com>
2025-07-18 06:03:40 +0500

    Portfolio commit

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,19 +1,31 @@
-import  Link  from 'next/link'
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className='flex'>
-      <p>Faheem Portfolio</p>
+      <div className="navbar-left">
+        <p className="logo">Faheem Portfolio</p>
       </div>
-      <div className='flex'>
-        <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/contact">Contacts</Link>
-      </div>
-    </nav>
-  )
-}
 
-export default Navbar
+      <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+        Menu
+      </button>
+
+      {menuOpen && (
+        <div className="menu-links">
+          <Link href="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link href="/about" onClick={() => setMenuOpen(false)}>About</Link>
+          <Link href="/projects" onClick={() => setMenuOpen(false)}>Projects</Link>
+          <Link href="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
