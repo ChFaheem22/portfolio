444abe86ba7e9697c19574e22be915ebc5a016c5
ChFaheem22 <faheemchh779@gmail.com>
2025-07-29 03:52:30 -0700

    Initial commit: Portfolio Website

--- app/about/page.js
+++ app/about/page.js
@@ -12,7 +12,7 @@ const About = () => {
         <h1 className="h1">About Me</h1>
         <p>
           I&rsquo;m Faheem, a frontend developer with a passion for creating interactive and user-friendly web interfaces.
-          I&rsquo;m currently pursuing a <b>Bachelor of Science in Software Engineering</b>, where I&rsquo;m building a strong foundation in software development and problem-solving.
+          I&rsquo;m currently pursuing a <b>Bachelor of Science in Software Engineering</b>, were I&rsquo;m building a strong foundation in software development and problem-solving.
           Alongside my studies, I&rsquo;m actively expanding expanding my skills in modern web development by learning <b>React.js</b> and <b>Next.js</b>.
           I enjoy building responsive, accessible websites and constantly seek to improve both my design sense and technical abilities.
           My core technologies include <b> HTML, CSS, JavaScript, React,</b> and <b>Next.js</b>.
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,25 +1,47 @@
-const Contacts = () =>{
-    return(
-        <div className="contact">
-            <h1>Contact Me</h1>
-            <p>You can reach me via the following platforms:</p>
-            <div className="info">
-            <ul>
-                <li>
-                    📧 Email: <a href="mailto:faheemchh779@gmail.com" rel="noopener noreferrer">
-                        faheemchh779@gmail.com</a>
-                </li>
-                <li>
-                    💻 LinkedIn: <a href="https://linkedin.com/in/faheem-95772830b" target="_blank" rel="noopener noreferrer">
-                    https://linkedin.com/in/faheem-95772830b</a>
-                </li>
-                <li>
-                    🔗 GitHub: <a href="https://github.com/ChFaheem22" target="_blank" rel="noopener noreferrer">
-                    https://github.com/ChFaheem22</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    )
-}
-export default Contacts
+const Contacts = () => {
+  return (
+    <div className="contact">
+      <h1>Contact Me</h1>
+      <p>You can reach me via the following platforms:</p>
+
+      <div className="info">
+        <ul>
+          <li>
+            📧 Email:{" "}
+            <a
+              href="mailto:faheemchh779@gmail.com"
+              rel="noopener noreferrer"
+            >
+              faheemchh779@gmail.com
+            </a>
+          </li>
+
+          <li>
+            💻 LinkedIn:{" "}
+            <a
+              href="https://www.linkedin.com/in/faheem-95772830b"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.linkedin.com/in/faheem-95772830b
+            </a>
+          </li>
+
+          <li>
+            🔗 GitHub:{" "}
+            <a
+              href="https://github.com/ChFaheem22"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://github.com/ChFaheem22
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
+
